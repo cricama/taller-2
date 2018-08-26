238502920b731c91e0efb9666da51b2fc8dfc560
cricama <31070308+cricama@users.noreply.github.com>
2018-08-26 18:48:23 -0500

    Add files via upload

---
+++ punto2.java
@@ -0,0 +1,39 @@
+package punto2;
+import java.util.Scanner;
+class punto2 {
+
+	public static void main(String[] args) {
+		
+			
+				Scanner sc = new Scanner (System.in);
+				
+				 int a=5;
+			      int b=2;
+			      int j=4;
+			      int doble,cuadrupe,quintuple;
+			      
+			      System.out.println("INGRESE EL NUMERO QUE QUIERE: ");
+			     
+			      a = sc.nextInt();
+			     
+			      doble = a*b;
+			    cuadrupe = a*j;
+			     quintuple = a*5;
+
+			      System.out.println("El doble del numero ( " + a + ") es: " + doble );
+		   
+			      System.out.println("El cuadrupe del numero ( " + a + ") es: " + cuadrupe );
+			      
+			      System.out.println("El quintuple del numero ( " + a + ") es: " + quintuple );
+		      
+			}
+			
+		}
+
+		
+		
+		
+		
+		
+
+
